2298dafdba65ff7f38a7df183abfab4b55dc5bf0
Jason Hite <hite77@gmail.com>
2023-02-08 21:17:38 -0500

    Part 1

--- day4/lib/day4.ex
+++ day4/lib/day4.ex
@@ -3,16 +3,48 @@ defmodule Day4 do
   Documentation for `Day4`.
   """
 
-  @doc """
-  Hello world.
+   @doc """
+  Split coordinates
 
   ## Examples
 
-      iex> Day4.hello()
-      :world
+      iex> Day4.splitCoordinates("6-6,4-6")
+      [6, 6, 4, 6]
 
   """
-  def hello do
-    :world
+  def splitCoordinates(line) do
+    [first, second] = String.split(line, ",")
+    [[startFirst, endFirst], [startSecond, endSecond]] = [String.split(first, "-"), String.split(second, "-")]
+    [String.to_integer(startFirst), String.to_integer(endFirst), String.to_integer(startSecond), String.to_integer(endSecond)]
+  end
+
+   @doc """
+  Split coordinates
+
+  ## Examples
+
+      iex> Day4.includes([6,6,4,6])
+      1
+
+      iex> Day4.includes([2,8,3,7])
+      1
+
+      iex> Day4.includes([2,4,6,8])
+      0
+
+
+  """
+
+  def includes([startFirst, endFirst, startSecond, endSecond]) when startFirst >= startSecond and endFirst <= endSecond, do: 1
+  def includes([startFirst, endFirst, startSecond, endSecond]) when startFirst <= startSecond and endFirst >= endSecond, do: 1
+  def includes(_), do: 0
+
+
+  def part1() do
+    {:ok, contents} = File.read("day4.txt")
+    contents |> String.split("\n", trim: false)
+             |> Enum.map(fn line -> splitCoordinates(line) end)
+             |> Enum.map(fn coords -> includes(coords) end)
+             |> Enum.sum()
   end
 end
--- day4/test/day4_test.exs
+++ day4/test/day4_test.exs
@@ -2,7 +2,16 @@ defmodule Day4Test do
   use ExUnit.Case
   doctest Day4
 
-  test "greets the world" do
-    assert Day4.hello() == :world
+  test "split" do
+    assert Day4.splitCoordinates("2-4,6-8") == [2,4,6,8]
+  end
+
+  test "fully includes ranges" do
+    assert Day4.includes([2,4,6,8]) == 0
+    assert Day4.includes([2,3,4,5]) == 0
+    assert Day4.includes([5,7,7,9]) == 0
+    assert Day4.includes([2,8,3,7]) == 1
+    assert Day4.includes([6,6,4,6]) == 1
+    assert Day4.includes([2,6,4,8]) == 0
   end
 end
