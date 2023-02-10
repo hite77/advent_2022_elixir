f7276d7bd91fd680d3639cbcf96f26497f334439
Jason Hite <hite77@gmail.com>
2023-02-09 22:29:37 -0500

    A little further along

--- day5/lib/day5.ex
+++ day5/lib/day5.ex
@@ -15,4 +15,72 @@ defmodule Day5 do
   def hello do
     :world
   end
+
+
+  def solve1 do
+    {:ok, contents} = File.read("day5.txt")
+    part1(contents)
+  end
+
+   #[head | tail] = sample[1]
+
+    #sample = put_in sample[1], tail
+    #sample = put_in sample[2], [ head | sample[2] ]
+
+  def sample do
+    sample2 = """
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+    # sample |> String.split("\n", trim: false)
+    # ["    [D]", "[N] [C]", "[Z] [M] [P]", " 1   2   3", "", "move 1 from 2 to 1",
+    #      "move 3 from 1 to 3", "move 2 from 2 to 1", "move 1 from 1 to 2", ""]
+    # get sample pulling moves and crates into separate objects.
+  end
+
+  def performMoves(moves, start) do
+    Enum.reduce(moves, start, fn (move, output) -> performMove(move, output) end)
+  end
+
+  def performMove({0, _from, _to }, output) do
+    output
+  end
+
+  def performMove({count, from, to}, output) do
+    performMove({count-1, from, to}, singleMove(output, from, to))
+  end
+
+  def singleMove(output, from, to) do
+    [head | tail] = output[from]
+    output = put_in output[from], tail
+    output = put_in output[to], [ head | output[to] ]
+    output
+  end
+
+  def topCrates(crates) do
+    Enum.join(Enum.map(crates, fn x -> Day5.topCrate(x) end))
+  end
+
+  def topCrate(crate) do
+    {_, ["[" <> almostStripped | _tail]} = crate
+    String.slice(almostStripped,0,1)
+  end
+
+  # document this
+  # test this with sample text.
+  def part1(contents) do
+    contents |> String.split("\n", trim: false) |> String.split("", trim: true)
+    # need to get the crates into map
+    # need to get moves into tuples array
+
+    # call perform moves
+    # call topCrates on it --> will output letters
+  end
 end
--- day5/test/day5_test.exs
+++ day5/test/day5_test.exs
@@ -2,7 +2,78 @@ defmodule Day5Test do
   use ExUnit.Case
   doctest Day5
 
-  test "greets the world" do
-    assert Day5.hello() == :world
+  test "perform moves will move the pieces" do
+    crates = %{
+      1 => ["[N]","[Z]"],
+      2 => ["[D]","[C]","[M]"],
+      3 => ["[P]"]
+    }
+
+    afterFirstMove = %{
+      1 => ["[D]","[N]","[Z]"],
+      2 => ["[C]","[M]"],
+      3 => ["[P]"]
+    }
+
+    afterSecondMove = %{
+      1 => [],
+      2 => ["[C]","[M]"],
+      3 => ["[Z]","[N]","[D]","[P]"]
+    }
+
+    afterThirdMove = %{
+      1 => ["[M]", "[C]"],
+      2 => [],
+      3 => ["[Z]","[N]","[D]","[P]"]
+    }
+
+    afterFourthMove = %{
+      1 => ["[C]"],
+      2 => ["[M]"],
+      3 => ["[Z]","[N]","[D]","[P]"]
+    }
+
+    #move 1 from 2 to 1
+    assert Day5.performMoves([{1, 2, 1}], crates) == afterFirstMove
+    #move 3 from 1 to 3
+    assert Day5.performMoves([{1, 2, 1},
+                              {3, 1, 3}], crates) == afterSecondMove
+    #move 2 from 2 to 1
+    assert Day5.performMoves([{1, 2, 1},
+                              {3, 1, 3},
+                              {2, 2, 1}], crates) == afterThirdMove
+    #move 1 from 1 to 2
+    assert Day5.performMoves([{1, 2, 1},
+                              {3, 1, 3},
+                              {2, 2, 1},
+                              {1, 1, 2}], crates) == afterFourthMove
   end
+
+  test "Gets top crates answer" do
+    afterFourthMove = %{
+      1 => ["[C]"],
+      2 => ["[M]"],
+      3 => ["[Z]","[N]","[D]","[P]"]
+    }
+
+    assert afterFourthMove |> Day5.topCrates() == "CMZ"
+  end
+
+  #     [D]
+  # [N] [C]
+  # [Z] [M] [P]
+  #  1   2   3   bottom
+
+  # move 1 from 2 to 1
+  # move 3 from 1 to 3
+  # move 2 from 2 to 1
+  # move 1 from 1 to 2
+
+  # After all this the top crate will be CMZ
+
+  # need to read the file in.....
+  #build the crate positions (3 columns here, 9 for input)
+  #positioning sideways should be parsed also may need to split at characters widths
+  #empties should not be added in....
+
 end
