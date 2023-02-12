6090b80a0f342cb0318a553faa0c53d55a10fdbb
Jason Hite <hite77@gmail.com>
2023-02-12 12:13:53 -0500

    Part 1 day 5 done

--- day5/lib/day5.ex
+++ day5/lib/day5.ex
@@ -3,59 +3,136 @@ defmodule Day5 do
   Documentation for `Day5`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day5.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
-
   def solve1 do
     {:ok, contents} = File.read("day5.txt")
     part1(contents)
   end
 
-   #[head | tail] = sample[1]
+  def separate("", output), do: output
+  def separate(item, []), do: [[item]]
+  def separate(item, [[head]]), do: [[item, head]]
+  def separate(item, [[head | tail]]) do
+    if !String.contains?(item, "[") && !String.contains?(item, "move") do
+      [[],[head | tail]]
+    else
+      [[item, head | tail]]
+    end
+  end
+  def separate(item, [[],moves]), do: [[item], moves]
+  def separate(item, [[crate],moves]), do: [[item, crate], moves]
+  def separate(item, [[head | tail],moves]), do: [[item, head | tail], moves]
 
-    #sample = put_in sample[1], tail
-    #sample = put_in sample[2], [ head | sample[2] ]
+@doc """
+  Convert Crates from string to map
 
-  def sample do
-    sample2 = """
-    [D]
-[N] [C]
-[Z] [M] [P]
- 1   2   3
+  ## Examples
 
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2
-"""
-    # sample |> String.split("\n", trim: false)
-    # ["    [D]", "[N] [C]", "[Z] [M] [P]", " 1   2   3", "", "move 1 from 2 to 1",
-    #      "move 3 from 1 to 3", "move 2 from 2 to 1", "move 1 from 1 to 2", ""]
-    # get sample pulling moves and crates into separate objects.
+  Statement for no string, leave collection alone
+  iex> Day5.convertCrates("", 1, %{})
+  %{}
+
+  non matching key will be added
+  iex> Day5.convertCrates("[J]", 5, %{1 => ["[N]"], 2 => ["[D]"], 3 => ["[P]"]})
+  %{1 => ["[N]"], 2 => ["[D]"], 3 => ["[P]"], 5 => ["[J]"]}
+
+
+  adding a crate to empty map
+  iex> Day5.convertCrates("[K]", 7, %{})
+  %{7 => ["[K]"]}
+
+  #cases for breaking a list down that has three columns.
+  iex> Day5.convertCrates("[K] [J] [I]", 1, %{1 => ["[A]","[B]","[C]"], 2 => ["[H]"]})
+  %{1 => ["[K]", "[A]", "[B]", "[C]"], 2 => ["[J]", "[H]"], 3 => ["[I]"]}
+
+  #and case for second column crate "    [D]"
+  iex> Day5.convertCrates("    [D]", 1, %{1 => ["[A]","[B]","[C]"], 2 => ["[H]"]})
+  %{1 => ["[A]", "[B]", "[C]"], 2 => ["[D]", "[H]"]}
+
+  Combines correctly
+  iex> Day5.convertCrates("[J]", 2, %{1 => ["[N]"], 2 => ["[D]"], 3 => ["[P]"]})
+  %{1 => ["[N]"], 2 => ["[J]", "[D]"], 3 => ["[P]"]}
+
+  iex> Day5.convertCrates("[K]", 3, %{1 => ["[N]"], 2 => ["[D]"], 3 => ["[P]", "[R]", "[Z]"]})
+  %{1 => ["[N]"], 2 => ["[D]"], 3 => ["[K]", "[P]", "[R]", "[Z]"]}
+
+  iex> Day5.convertCrates("[A]", 1, %{1 => ["[N]", "[O]"], 2 => ["[D]"], 3 => ["[P]", "[R]", "[Z]"]})
+  %{1 => ["[A]","[N]","[O]"], 2 => ["[D]"], 3 => ["[P]", "[R]","[Z]"]}
+
+  ignores empty entry and returns existing map
+  iex> Day5.convertCrates("   ", 1, %{})
+  %{}
+
+  """
+
+  def convertCrates("", _column, crateMap), do: crateMap
+  def convertCrates("   ", _column, crateMap), do: crateMap
+  def convertCrates(crateToAdd, column, crateMap) when byte_size(crateToAdd) == 3 and is_map_key(crateMap, column) do
+    [head | tail] = crateMap[column]
+    put_in crateMap[column], [crateToAdd, head | tail]
   end
 
-  def performMoves(moves, start) do
-    Enum.reduce(moves, start, fn (move, output) -> performMove(move, output) end)
+  def convertCrates(crateToAdd, column, crateMap) when byte_size(crateToAdd) == 3 and is_map_key(crateMap, column) == false, do: put_in crateMap[column], [crateToAdd]
+  def convertCrates(cratesToAdd, column, crateMap) when byte_size(cratesToAdd) > 3 do
+    {entry, newRemainder} = String.split_at(cratesToAdd, 4)
+    trimmedEntry = String.trim(entry)
+    convertCrates(newRemainder, column + 1, convertCrates(trimmedEntry, column, crateMap))
   end
 
-  def performMove({0, _from, _to }, output) do
-    output
+
+
+  @doc """
+  Perform move calls single move to move the indicated number of times
+
+  ## Example
+
+  iex> Day5.convertMove("move 9 from 14 to 8")
+  {9, 14, 8}
+
+  """
+
+  def convertMove(moveString) do
+    ["move " <> countString, unparsed] = String.split(moveString, " from ")
+    count = String.to_integer(countString)
+    [fromString, toString] = String.split(unparsed, " to ")
+    from = String.to_integer(fromString)
+    to = String.to_integer(toString)
+    {count, from, to}
   end
 
+  @doc """
+  Perform move calls single move to move the indicated number of times
+
+  ## Examples
+
+  Zero moves will return same as input
+  iex> Day5.performMove({0, 2, 1}, %{1 => ["[N]","[Z]"],2 => ["[D]","[C]","[M]"],3 => ["[P]"]})
+  %{1 => ["[N]","[Z]"],2 => ["[D]","[C]","[M]"],3 => ["[P]"]}
+
+  Move all three from column 2 to column 1
+  iex> Day5.performMove({3, 2, 1}, %{1 => ["[N]","[Z]"],2 => ["[D]","[C]","[M]"],3 => ["[P]"]})
+  %{1 => ["[M]", "[C]", "[D]", "[N]", "[Z]"], 2 => [], 3 => ["[P]"]}
+
+  """
+
+  def performMove({count, _from, _to }, output) when count == 0, do: output
   def performMove({count, from, to}, output) do
     performMove({count-1, from, to}, singleMove(output, from, to))
   end
+
+   @doc """
+  Single Move moves a crate from the from and puts it in to
+
+  ## Examples
+
+  Moving crate from 2 column to 1st column
+  iex> Day5.singleMove(%{1 => ["[N]","[Z]"],2 => ["[D]","[C]","[M]"],3 => ["[P]"]}, 2, 1)
+  %{1 => ["[D]", "[N]", "[Z]"], 2 => ["[C]", "[M]"], 3 => ["[P]"]}
+
+  Move from 3 to 1 emptying 3rd column
+  iex> Day5.singleMove(%{1 => ["[D]", "[N]", "[Z]"], 2 => ["[C]", "[M]"], 3 => ["[P]"]}, 3, 2)
+  %{1 => ["[D]", "[N]", "[Z]"], 2 => ["[P]", "[C]", "[M]"], 3 => []}
+
+  """
 
   def singleMove(output, from, to) do
     [head | tail] = output[from]
@@ -64,23 +141,47 @@ move 1 from 1 to 2
     output
   end
 
-  def topCrates(crates) do
-    Enum.join(Enum.map(crates, fn x -> Day5.topCrate(x) end))
-  end
+  @doc """
+  Top Crate pulls the left most item
 
+  ## Examples
+
+  One item will return the letter in the crate.
+  iex> Day5.topCrate({1, ["[A]"]})
+  "A"
+
+  More then one item will return the first letter in the column.
+  iex> Day5.topCrate({1, ["[J]", "[C]", "[H]"]})
+  "J"
+  """
   def topCrate(crate) do
-    {_, ["[" <> almostStripped | _tail]} = crate
-    String.slice(almostStripped,0,1)
+    {_, ["[" <> characterAndClosingBrace | _tail]} = crate
+    "]" <> stripped = String.reverse(characterAndClosingBrace)
+    stripped
   end
 
-  # document this
-  # test this with sample text.
-  def part1(contents) do
-    contents |> String.split("\n", trim: false) |> String.split("", trim: true)
-    # need to get the crates into map
-    # need to get moves into tuples array
+    @doc ~S"""
+  Part 1 -- Parses contents for Crate locations at start, moves to perform
+  It makes the moves, and then pulls the letters for top crate as answer
 
-    # call perform moves
-    # call topCrates on it --> will output letters
+  ## Example
+
+  One item will return the letter in the crate.
+  iex> Day5.part1("    [D]\n[N] [C]\n[Z] [M] [P]\n 1   2   3\n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2\n")
+  "CMZ"
+
+  """
+
+  def part1(contents) do
+    [movesStrings, cratesString] = contents |> String.split("\n", trim: false)
+                                            |> Enum.reduce([], fn (item, output) -> separate(item, output) end)
+
+    crates = cratesString |> Enum.reduce(%{}, fn (string, crates) -> convertCrates(string, 1 ,crates) end)
+
+    movesStrings |> Enum.reverse()
+                 |> Enum.map(fn moveString -> convertMove(moveString) end)
+                 |> Enum.reduce(crates, fn (move, output) -> performMove(move, output) end)
+                 |> Enum.map(fn x -> Day5.topCrate(x) end)
+                 |> Enum.join()
   end
 end
--- day5/test/day5_test.exs
+++ day5/test/day5_test.exs
@@ -2,6 +2,30 @@ defmodule Day5Test do
   use ExUnit.Case
   doctest Day5
 
+  test "separate out crates and moves" do
+    input = ["    [D]", "[N] [C]", "[Z] [M] [P]", " 1   2   3", "", "move 1 from 2 to 1", "move 3 from 1 to 3", "move 2 from 2 to 1", "move 1 from 1 to 2", ""]
+    [moves, crates] = Enum.reduce(input, [], fn (item, output) -> Day5.separate(item, output) end)
+    assert crates == ["[Z] [M] [P]", "[N] [C]", "    [D]"]
+    assert moves ==  ["move 1 from 1 to 2", "move 2 from 2 to 1", "move 3 from 1 to 3", "move 1 from 2 to 1"]
+  end
+
+  test "converting from strings to map of crates" do
+   assert Day5.convertCrates("    [D]", 1, %{}) == %{2 => ["[D]"]}
+   assert Day5.convertCrates("[N] [C]", 1, %{2 => ["[D]"]}) == %{
+        1 => ["[N]"],
+        2 => ["[C]", "[D]"]
+      }
+    assert Day5.convertCrates("[Z] [M] [P]", 1, %{1 => ["[N]"], 2 => ["[C]", "[D]"]}) == %{
+        1 => ["[Z]", "[N]"],
+        2 => ["[M]", "[C]", "[D]"],
+        3 => ["[P]"]}
+  end
+
+  test "can convert moves to tuples" do
+    assert Day5.convertMove("move 1 from 2 to 1") == {1, 2, 1}
+    assert Day5.convertMove("move 3 from 1 to 3") == {3, 1, 3}
+  end
+
   test "perform moves will move the pieces" do
     crates = %{
       1 => ["[N]","[Z]"],
@@ -33,47 +57,34 @@ defmodule Day5Test do
       3 => ["[Z]","[N]","[D]","[P]"]
     }
 
-    #move 1 from 2 to 1
-    assert Day5.performMoves([{1, 2, 1}], crates) == afterFirstMove
-    #move 3 from 1 to 3
-    assert Day5.performMoves([{1, 2, 1},
-                              {3, 1, 3}], crates) == afterSecondMove
-    #move 2 from 2 to 1
-    assert Day5.performMoves([{1, 2, 1},
-                              {3, 1, 3},
-                              {2, 2, 1}], crates) == afterThirdMove
-    #move 1 from 1 to 2
-    assert Day5.performMoves([{1, 2, 1},
-                              {3, 1, 3},
-                              {2, 2, 1},
-                              {1, 1, 2}], crates) == afterFourthMove
+    assert Day5.performMove({1, 2, 1}, crates) == afterFirstMove
+    assert Day5.performMove({3, 1, 3}, afterFirstMove) == afterSecondMove
+    assert Day5.performMove({2, 2, 1}, afterSecondMove) == afterThirdMove
+    assert Day5.performMove({1, 1, 2}, afterThirdMove) == afterFourthMove
   end
 
-  test "Gets top crates answer" do
-    afterFourthMove = %{
-      1 => ["[C]"],
-      2 => ["[M]"],
-      3 => ["[Z]","[N]","[D]","[P]"]
-    }
-
-    assert afterFourthMove |> Day5.topCrates() == "CMZ"
+  test "Gets top crate" do
+    assert Day5.topCrate({1, ["[C]"]}) == "C"
+    assert Day5.topCrate({1, ["[M]"]}) == "M"
+    assert Day5.topCrate({1, ["[Z]","[N]","[D]","[P]"]}) == "Z"
   end
 
-  #     [D]
-  # [N] [C]
-  # [Z] [M] [P]
-  #  1   2   3   bottom
+  test "part1 test" do
+    input = """
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
 
-  # move 1 from 2 to 1
-  # move 3 from 1 to 3
-  # move 2 from 2 to 1
-  # move 1 from 1 to 2
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+    assert Day5.part1(input) == "CMZ"
+  end
 
-  # After all this the top crate will be CMZ
-
-  # need to read the file in.....
-  #build the crate positions (3 columns here, 9 for input)
-  #positioning sideways should be parsed also may need to split at characters widths
-  #empties should not be added in....
-
+  test "solve1 using my input" do
+    assert Day5.solve1 == "TDCHVHJTG"
+  end
 end
