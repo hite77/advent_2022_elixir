6bd1236d9d5f95a29c16eaa00c18f82e777fd1f1
Jason Hite <hite77@gmail.com>
2023-02-13 11:28:28 -0500

    First part of day 6

--- day6/lib/day6.ex
+++ day6/lib/day6.ex
@@ -4,15 +4,31 @@ defmodule Day6 do
   """
 
   @doc """
-  Hello world.
+  Find position of first unique set of four characters
 
   ## Examples
 
-      iex> Day6.hello()
-      :world
+      iex> Day6.findPositionOfFirstUniqueFourCharacters("abcd", 4)
+      4
 
+      iex> Day6.findPositionOfFirstUniqueFourCharacters("abcad", 4)
+      5
+
+      iex> Day6.findPositionOfFirstUniqueFourCharacters("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4)
+      7
   """
-  def hello do
-    :world
+
+  def findPositionOfFirstUniqueFourCharacters(<<first::binary-size(1)>> <>
+  <<second::binary-size(1)>> <>
+  <<third::binary-size(1)>> <>
+  <<fourth::binary-size(1)>> <> _rest, count) when first != second and first != third and first != fourth and second != third and second != fourth and third != fourth, do: count
+  def findPositionOfFirstUniqueFourCharacters(<<_first::binary-size(1)>> <>
+  <<second::binary-size(1)>> <>
+  <<third::binary-size(1)>> <>
+  <<fourth::binary-size(1)>> <> rest, count), do: findPositionOfFirstUniqueFourCharacters(second <> third <> fourth <> rest, count + 1)
+
+  def solve1 do
+    {:ok, contents} = File.read("day6.txt")
+    findPositionOfFirstUniqueFourCharacters(contents, 4)
   end
 end
--- day6/test/day6_test.exs
+++ day6/test/day6_test.exs
@@ -2,7 +2,15 @@ defmodule Day6Test do
   use ExUnit.Case
   doctest Day6
 
-  test "greets the world" do
-    assert Day6.hello() == :world
+  test "finds location of non repeat of four characters" do
+    assert Day6.findPositionOfFirstUniqueFourCharacters("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4) == 7
+    assert Day6.findPositionOfFirstUniqueFourCharacters("bvwbjplbgvbhsrlpgdmjqwftvncz", 4) == 5
+    assert Day6.findPositionOfFirstUniqueFourCharacters("nppdvjthqldpwncqszvftbrmjlhg", 4) == 6
+    assert Day6.findPositionOfFirstUniqueFourCharacters("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4) == 10
+    assert Day6.findPositionOfFirstUniqueFourCharacters("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4) == 11
+  end
+
+  test "solve part 1" do
+    assert Day6.solve1 == 1760
   end
 end
