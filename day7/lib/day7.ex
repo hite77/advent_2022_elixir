1821714ecfd0d6dac3ba9c3c2e3ba95452e64f44
Jason Hite <hite77@gmail.com>
2023-02-15 14:04:52 -0500

    Finished part1 day7

--- day7/lib/day7.ex
+++ day7/lib/day7.ex
@@ -1,18 +1,193 @@
 defmodule Day7 do
   @moduledoc """
-  Documentation for `Day7`.
+  Free up space advent exercise
   """
 
   @doc """
-  Hello world.
+  Parse out directories and files
+
+  ## Examples
+  Ignore ""
+  iex> Day7.parsing("", [[], %{"/" => [{"/a"}, {29116, "f"}]}])
+  [[], %{"/" => [{"/a"}, {29116, "f"}]}]
+
+  Ignore "$ ls"
+  iex> Day7.parsing("$ ls", "anything")
+  "anything"
+
+  Set path which is stored at beginning
+  iex> Day7.parsing("$ cd /", [["a","e"], %{"/a" => [{"/a/e"}]}])
+  [[], %{"/a" => [{"/a/e"}]}]
+
+  Set path to cd ..
+  iex> Day7.parsing("$ cd ..", [["a","e"], %{"/a" => [{"/a/e"}]}])
+  [["a"], %{"/a" => [{"/a/e"}]}]
+
+  Set path from /
+  iex> Day7.parsing("$ cd e", [[], %{"/a" => [{"/a/e"}]}])
+  [["e"], %{"/a" => [{"/a/e"}]}]
+
+  Set path to name
+  iex> Day7.parsing("$ cd e", [["a"], %{"/a" => [{"/a/e"}]}])
+  [["a", "e"], %{"/a" => [{"/a/e"}]}]
+
+  Set path to name with more dirs
+  iex> Day7.parsing("$ cd f", [["a", "e"], %{"/a" => [{"/a/e"}]}])
+  [["a", "e", "f"], %{"/a" => [{"/a/e"}]}]
+
+  Add directory to empty map
+  iex> Day7.parsing("dir jason", [[], %{}])
+  [[], %{"/" => [{"/jason"}]}]
+
+  Add directory to empty map
+  iex> Day7.parsing("dir e", [["a"], %{}])
+  [["a"], %{"/a" => [{"/a/e"}]}]
+
+  Add directory nested to empty map
+  iex> Day7.parsing("dir f", [["a", "b"], %{}])
+  [["a", "b"], %{"/a/b" => [{"/a/b/f"}]}]
+
+  Add directory to other entries
+  iex> Day7.parsing("dir g", [["a","e"], %{"/a/e" => [{43, "file"}]}])
+  [["a", "e"], %{"/a/e" => [{43, "file"}, {"/a/e/g"}]}]
+
+  Add file size to empty map
+  iex> Day7.parsing("29116 f", [["a"], %{}])
+  [["a"], %{"/a" => [{29116, "f"}]}]
+
+  Add file size nested to empty map
+  iex> Day7.parsing("134 t.txt", [["y", "z"], %{}])
+  [["y", "z"], %{"/y/z" => [{134, "t.txt"}]}]
+
+  Add file size to other entries
+  iex> Day7.parsing("35643 z.txt", [["c","d"], %{"/a/b" => [{24, "another"}], "/c/d" => [{43, "file"}]}])
+  [["c", "d"], %{"/a/b" => [{24, "another"}], "/c/d" => [{43, "file"}, {35643, "z.txt"}]}]
+
+  """
+  def parsing("", files), do: files
+  def parsing("$ ls", files), do: files
+  def parsing("$ cd /", [[_head | _tail], map]), do: [[], map]
+  def parsing("$ cd /", [[], map]), do: [[], map]
+  def parsing("$ cd ..", [[_singleDirectory], map]), do: [[], map]
+  def parsing("$ cd ..", [[head | tail], map]), do: [removeLast(head, tail), map]
+  def parsing("$ cd " <> directory, [[] , map]), do: [[directory] , map]
+  def parsing("$ cd " <> directory, [[head | tail] , map]), do: [append(head, tail, directory) , map]
+  def parsing("dir " <> directory, [[] , map]) do
+    addDir("/", map, directory, [])
+  end
+  def parsing("dir " <> directory, [[head | tail] , map]) do
+    addDir("/" <> Enum.join([head | tail], "/"), map, directory, [head | tail])
+  end
+  def parsing(item, [[], map]) do
+    addSize("/", map, item, [])
+  end
+  def parsing(item, [[head | tail] , map]) do
+    addSize("/" <> Enum.join([head | tail], "/"), map, item, [head | tail])
+  end
+
+  def removeLast(head, tail) do
+    [head | tail |> :lists.reverse() |> tl() |> :lists.reverse()]
+  end
+
+  def addDirectoryToPath("/", directory), do: "/" <> directory
+  def addDirectoryToPath(path, directory), do: path <> "/" <> directory
+
+  def addDir(pathString, map, directory, path) do
+    addItem(pathString, map, [{addDirectoryToPath(pathString, directory)}], path)
+  end
+
+  def addSize(pathString, map, item, path) do
+    [size, filename] = String.split(item, " ")
+    sizeTuple = {String.to_integer(size), filename}
+    addItem(pathString, map, [sizeTuple], path)
+  end
+
+  def addItem(pathString, map, item, path) do
+    if is_map_key(map, pathString) do
+      [path, put_in(map[pathString], Enum.concat(map[pathString], item))]
+    else
+      [path, put_in(map[pathString], item)]
+    end
+  end
+
+
+  @doc """
+  Parse out directories and files
+
+  ## Examples
+  Ignore ""
+  iex> Day7.append("a", ["b","c"], "d")
+  ["a", "b", "c", "d"]
+
+  """
+  def append(head, tail, add) do
+    ender = tail |> Enum.reverse()
+    added = [add | ender] |> Enum.reverse()
+    [head | added]
+  end
+
+  @doc """
+  Add up sizes
 
   ## Examples
 
-      iex> Day7.hello()
-      :world
+  Single item less than 10000
+  iex> Day7.sizes({"/a", [{300, "a file"}]}, %{})
+  300
+
+  Two items added together
+  iex> Day7.sizes({"/e", [{350, "a file"}, {400, "b file"}]}, %{})
+  750
+
+  Nested folder added
+  iex> Day7.sizes({"/pathDir", [{"/a"}, {338, "a file"}, {750, "b file"}]}, %{"/a" => [{13, "c file"}]})
+  1101
+
+  One item as folder but leads to other folders
+  iex> Day7.sizes({"/folderInside", [{"/another"}]}, %{"/another" => [{13, "c file"}, {"/c"}], "/c" => [{39, "d file"}]})
+  52
 
   """
-  def hello do
-    :world
+
+
+  def sizes({_path, []}, _files), do: 0
+  def sizes({_path, [{size, _filename}]}, _files), do: size
+  def sizes({path, [{size, _filename} | tail]}, files), do: size + sizes({path, tail}, files)
+  def sizes({path, [{directory} | tail]}, files), do: sizes({directory, files[directory]}, files) + sizes({path, tail}, files)
+
+  @doc """
+  Round off
+
+  ## Examples
+
+  item less than 100000
+  iex> Day7.truncate(99999)
+  99999
+
+  item equal to 100000
+  iex> Day7.truncate(100000)
+  100000
+
+  item greater than 100000 will be 0
+  iex> Day7.truncate(100001)
+  0
+
+  """
+
+  def truncate(size) when size > 100000, do: 0
+  def truncate(size), do: size
+
+  def part1(contents) do
+    [_path, map] = contents |> String.split("\n")
+                            |> Enum.reduce([[], %{}], fn (item, files) -> parsing(item, files) end)
+
+    map |> Enum.map(fn folder -> sizes(folder, map) end)
+        |> Enum.map(fn size -> truncate(size) end)
+        |> Enum.sum()
+  end
+
+  def solve1 do
+    {:ok, contents} = File.read("day7.txt")
+    part1(contents)
   end
 end
--- day7/test/day7_test.exs
+++ day7/test/day7_test.exs
@@ -2,7 +2,63 @@ defmodule Day7Test do
   use ExUnit.Case
   doctest Day7
 
-  test "greets the world" do
-    assert Day7.hello() == :world
+  test "ls keeps path and modal same" do
+    assert Day7.parsing("$ ls", [["a", "e"], %{"/a" => [{"/a/e"}]}]) == [["a", "e"], %{"/a" => [{"/a/e"}]}]
+  end
+
+  test "cd dir will work in several modes" do
+    assert Day7.parsing("$ cd e", [[], %{"/a" => [{"/a/e"}]}]) == [["e"], %{"/a" => [{"/a/e"}]}]
+    assert Day7.parsing("$ cd e", [["a"], %{"/a" => [{"/a/e"}]}]) == [["a", "e"], %{"/a" => [{"/a/e"}]}]
+    assert Day7.parsing("$ cd e", [["a", "b"], %{"/a" => [{"/a/e"}]}]) == [["a", "b", "e"], %{"/a" => [{"/a/e"}]}]
+    assert Day7.parsing("$ cd e", [["a", "b", "c"], %{"/a" => [{"/a/e"}]}]) == [["a", "b", "c", "e"], %{"/a" => [{"/a/e"}]}]
+  end
+
+  test "cd / willl work in several modes" do
+    assert Day7.parsing("$ cd /", [["a", "e"], %{"/a" => [{"/a/e"}]}]) == [[], %{"/a" => [{"/a/e"}]}]
+  end
+
+  test "cd .. will work in several modes" do
+    assert Day7.parsing("$ cd ..", [["a"], %{}]) == [[], %{}]
+    assert Day7.parsing("$ cd ..", [["a", "e"], %{"/a" => [{"/a/e"}]}]) == [["a"], %{"/a" => [{"/a/e"}]}]
+    assert Day7.parsing("$ cd ..", [["a", "e", "f"], %{"/a" => [{"/a/e"}]}]) == [["a", "e"], %{"/a" => [{"/a/e"}]}]
+    assert Day7.parsing("$ cd ..", [["a", "b", "c", "d"], %{"/a" => [{"/a/e"}]}]) == [["a", "b", "c"], %{"/a" => [{"/a/e"}]}]
+    assert Day7.parsing("$ cd ..", [["a", "b", "c", "d", "e"], %{"/a" => [{"/a/e"}]}]) == [["a", "b", "c", "d"], %{"/a" => [{"/a/e"}]}]
+  end
+
+  test "add item conditions" do
+    assert Day7.parsing("29116 f", [[], %{}]) == [[], %{"/" => [{29116, "f"}]}]
+    assert Day7.parsing("16116 z.txt", [["a"], %{}]) == [["a"], %{"/a" => [{16116, "z.txt"}]}]
+    assert Day7.parsing("42 rest.def", [["a", "b"], %{}]) == [["a", "b"], %{"/a/b" => [{42, "rest.def"}]}]
+    assert Day7.parsing("456 shrek.movie.really", [["a", "b", "c"], %{}]) == [["a", "b", "c"], %{"/a/b/c" => [{456, "shrek.movie.really"}]}]
+  end
+
+  test "add dir conditions" do
+    assert Day7.parsing("dir f", [[], %{}]) == [[], %{"/" => [{"/f"}]}]
+    assert Day7.parsing("dir zztop", [["a"], %{}]) == [["a"], %{"/a" => [{"/a/zztop"}]}]
+    assert Day7.parsing("dir something", [["a", "b"], %{}]) == [["a", "b"], %{"/a/b" => [{"/a/b/something"}]}]
+    assert Day7.parsing("dir works", [["a", "b", "c"], %{}]) == [["a", "b", "c"], %{"/a/b/c" => [{"/a/b/c/works"}]}]
+  end
+
+  test "parsing" do
+    input = "$ cd /\n$ ls\ndir a\n14848514 b.txt\n8504156 c.dat\ndir d\n$ cd a\n$ ls\ndir e\n29116 f\n2557 g\n62596 h.lst\n$ cd e\n$ ls\n584 i\n$ cd ..\n$ cd ..\n$ cd d\n$ ls\n4060174 j\n8033020 d.log\n5626152 d.ext\n7214296 k\n"
+    assert input |> String.split("\n")
+                 |> Enum.reduce([[], %{}], fn (item, files) -> Day7.parsing(item, files) end) ==
+                  [["d"], %{"/" => [{"/a"}, {14848514, "b.txt"}, {8504156, "c.dat"}, {"/d"}],
+                  "/a" => [{"/a/e"}, {29116, "f"}, {2557, "g"}, {62596, "h.lst"}],
+                  "/a/e" => [{584, "i"}],
+                  "/d" => [{4060174, "j"}, {8033020, "d.log"}, {5626152, "d.ext"}, {7214296, "k"}]}]
+  end
+
+  test "part 1" do
+    input = "$ cd /\n$ ls\ndir a\n14848514 b.txt\n8504156 c.dat\ndir d\n$ cd a\n$ ls\ndir e\n29116 f\n2557 g\n62596 h.lst\n$ cd e\n$ ls\n584 i\n$ cd ..\n$ cd ..\n$ cd d\n$ ls\n4060174 j\n8033020 d.log\n5626152 d.ext\n7214296 k\n"
+    assert Day7.part1(input) == 95437
+  end
+
+  test "adding up sizes of directories up to 100000" do
+    assert Day7.sizes({"/a/e", [{584, "i"}]}, %{}) == 584
+  end
+
+  test "solve" do
+    assert Day7.solve1 == 1642503
   end
 end
