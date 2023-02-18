980f59e6fadddb800e343649b33f43cd3ee73a74
Jason Hite <hite77@gmail.com>
2023-02-17 22:04:33 -0500

    Part 1 of day 9 done

--- day9/README.md
+++ day9/README.md
@@ -1,21 +1,81 @@
 # Day9
 
-**TODO: Add description**
+**Part 1**
 
-## Installation
+-rope is short (H) and (T) must always be touching
+-diagnonally adjacent, or overlapping
+```
+...
+.TH.
+....
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `day9` to your list of dependencies in `mix.exs`:
+....
+.H..
+..T.
+....
 
-```elixir
-def deps do
-  [
-    {:day9, "~> 0.1.0"}
-  ]
-end
+...
+.H. (H covers T)
+...
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/day9>.
+- if the head is ever two steps directly up, down, left, or right from the tail, the tail must also move one step in that direction so it remains close enough.
+```
+.....    .....    .....
+.TH.. -> .T.H. -> ..TH.
+.....    .....    .....
 
+...    ...    ...
+.T.    .T.    ...
+.H. -> ... -> .T.
+...    .H.    .H.
+...    ...    ...
+```
+-otherwise, if the head and tail aren’t touching and aren’t in the same row or column, the tail always moves one step diagonally to keep up:
+
+```
+.....    .....    .....
+.....    ..H..    ..H..
+..H.. -> ..... -> ..T..
+.T...    .T...    .....
+.....    .....    .....
+
+.....    .....    .....
+.....    .....    .....
+..H.. -> ...H. -> ..TH.
+.T...    .T...    .....
+.....    .....    .....
+```
+
+- at start they are overlapped
+- then head follows a series of moves
+```
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
+```
+- so this represents
+1 head right 4
+2 then up 4
+3 then left 3
+4 then down 1
+5 right 4
+6 down 1
+7 left 5
+8 right 2
+
+- move H with Tail following
+- count up the positions the tail took up for this it is 13
+
+**steps**
+— parse arguments into move list
+— track current position of H and T
+— as H moves — find out if T moves (move one step at a time)
+— T will need to update and add position
+— as T moves add unique positions to list (no duplicates). map? “1,1” => count of visits may need counts for part 2, or don’t throw away.
+— for part 1 count up resultant maps keys.
--- day9/lib/day9.ex
+++ day9/lib/day9.ex
@@ -4,15 +4,121 @@ defmodule Day9 do
   """
 
   @doc """
-  Hello world.
+  Put count of coordinate in map or update count
 
   ## Examples
 
-      iex> Day9.hello()
-      :world
+      iex> Day9.updateMap(1,5, %{})
+      %{"1,5" => 1}
+
+      iex> Day9.updateMap(3,8, %{"3,8" => 5})
+      %{"3,8" => 6}
 
   """
-  def hello do
-    :world
+  def updateMap(x, y, map) do
+    if is_map_key(map, "#{x},#{y}") do
+      %{map | "#{x},#{y}" => map["#{x},#{y}"] + 1}
+    else
+      Map.put(map, "#{x},#{y}", 1)
+    end
+  end
+
+  @doc """
+  Distance between
+
+  ## Examples
+
+      iex> Day9.distanceBetween(-5, -4)
+      1
+
+      iex> Day9.distanceBetween(-4, -5)
+      1
+
+      iex> Day9.distanceBetween(-4, 5)
+      9
+
+      iex> Day9.distanceBetween(5, -5)
+      10
+
+      iex> Day9.distanceBetween(-1, 1)
+      2
+
+      iex> Day9.distanceBetween(1, -1)
+      2
+  """
+  def distanceBetween(first, second) do
+    #within 2 -- both neg, both positive, 1 and -1 should be 2
+    #find bigger number? then subtract smaller from bigger?
+    if first > second do
+      abs(first-second)
+    else
+      abs(second-first)
+    end
+
+  end
+
+  #TODO: document based on subset of tests
+  def performMove("R", [leadx, leady, followx, followy, map]) do
+    distancex = distanceBetween(leadx+1,followx)
+    distancey = distanceBetween(leady,followy)
+    cond do
+      (distancex + distancey <= 1) or # only one away means left/right/top/bottom
+      (distancex == 1 and distancey == 1) -> [leadx+1, leady, followx, followy, map]
+      followy > leady -> [leadx+1, leady, followx+1, followy-1, updateMap(followx+1, followy-1, map) ]
+      followy < leady -> [leadx+1, leady, followx+1, followy+1, updateMap(followx+1, followy+1, map) ]
+      true            -> [leadx+1, leady, followx+1, followy  , updateMap(followx+1, followy  , map) ]
+    end
+  end
+  def performMove("L", [leadx, leady, followx, followy, map]) do
+    distancex = distanceBetween(leadx-1,followx)
+    distancey = distanceBetween(leady,followy)
+    cond do
+      (distancex + distancey <= 1) or # only one away means left/right/top/bottom
+      (distancex == 1 and distancey == 1) -> [leadx-1, leady, followx, followy, map]
+      followy > leady -> [leadx-1, leady, followx-1, followy-1, updateMap(followx-1, followy-1, map) ]
+      followy < leady -> [leadx-1, leady, followx-1, followy+1, updateMap(followx-1, followy+1, map) ]
+      true            -> [leadx-1, leady, followx-1, followy  , updateMap(followx-1, followy  , map) ]
+    end
+  end
+  def performMove("U", [leadx, leady, followx, followy, map]) do
+    distancex = distanceBetween(leadx,followx)
+    distancey = distanceBetween(leady+1,followy)
+    cond do
+      (distancex + distancey <= 1) or # only one away means left/right/top/bottom
+      (distancex == 1 and distancey == 1) -> [leadx, leady+1, followx, followy, map]
+      followx > leadx -> [leadx, leady+1, followx-1, followy+1, updateMap(followx-1, followy+1, map) ]
+      followx < leadx -> [leadx, leady+1, followx+1, followy+1, updateMap(followx+1, followy+1, map) ]
+      true            -> [leadx, leady+1, followx  , followy+1, updateMap(followx  , followy+1, map) ]
+    end
+  end
+  def performMove("D", [leadx, leady, followx, followy, map]) do
+    distancex = distanceBetween(leadx,followx)
+    distancey = distanceBetween(leady-1,followy)
+    cond do
+      (distancex + distancey <= 1) or # only one away means left/right/top/bottom
+      (distancex == 1 and distancey == 1) -> [leadx, leady-1, followx, followy, map]
+      followx > leadx -> [leadx, leady-1, followx-1, followy-1, updateMap(followx-1, followy-1, map) ]
+      followx < leadx -> [leadx, leady-1, followx+1, followy-1, updateMap(followx+1, followy-1, map) ]
+      true            -> [leadx, leady-1, followx  , followy-1, updateMap(followx  , followy-1, map) ]
+    end
+  end
+
+  #TODO: document based on subset of tests
+  def unwrapMoves(_character, 1, output), do: output
+  def unwrapMoves(character, count, [head | tail]), do: unwrapMoves(character, count-1, [character, head | tail])
+  def unwrapMoves(<<character::binary-size(1)>> <> " " <> count), do: unwrapMoves(character, String.to_integer(count), [character])
+
+  #TODO: document with example
+  def part1(contents) do
+    [_leadx, _leady, _followx, _followy, map] = contents |> Enum.map(fn move -> unwrapMoves(move) end)
+                                                         |> Enum.flat_map(fn move -> move end)
+                                                         |> Enum.reduce([0,0,0,0, %{"0,0" => 1}], fn (move, data) -> performMove(move, data) end)
+    map |> Map.keys()
+        |> length()
+  end
+
+  def solve1 do
+    {:ok, contents} = File.read("day9.txt")
+    part1(String.split(contents,"\n"))
   end
 end
