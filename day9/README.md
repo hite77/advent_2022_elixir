7bd0d2877f29a3665357db120a4fb6f7fdd7c096
Jason Hite <hite77@gmail.com>
2023-02-19 17:48:23 -0500

    Finished with day 9

--- day9/README.md
+++ day9/README.md
@@ -1,6 +1,6 @@
 # Day9
 
-**Part 1**
+## Part 1
 
 -rope is short (H) and (T) must always be touching
 -diagnonally adjacent, or overlapping
@@ -79,3 +79,43 @@ R 2
 — T will need to update and add position
 — as T moves add unique positions to list (no duplicates). map? “1,1” => count of visits may need counts for part 2, or don’t throw away.
 — for part 1 count up resultant maps keys.
+
+scratchpad
+
+
+## Part 2
+
+-longer ropes
+-rather than 2 knots (HT) now there needs to be:
+-(H covers 1, 2, 3, 4, 5, 6, 7, 8, 9, s)
+-H, 1-9 are moving using the same rules
+-track where 9 moves
+
+- for the:
+
+```
+assert Day9.part2([“R 4","U 4","L 3","D 1","R 4","D 1","L 5","R 2"]) == 1
+```
+- the other pieces are detailed and should be able to integration test it.
+- also:
+```
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+```
+- is detailed at steps, and answer is: tail (9) visits 36 positions
+
+**thoughts**
+
+- store locations of H (already done)
+- refactor code so that movement of head happens separately for part 1 and part 2….
+- then calculate movement of T for part 1
+- then calculate movement of 1 for part 2
+- continue for 2 based on 1
+- continue for …9 based on 8
+- Perform integration
--- day9/lib/day9.ex
+++ day9/lib/day9.ex
@@ -110,15 +110,194 @@ defmodule Day9 do
 
   #TODO: document with example
   def part1(contents) do
-    [_leadx, _leady, _followx, _followy, map] = contents |> Enum.map(fn move -> unwrapMoves(move) end)
+    # should be able to refactor to move head and have one reducer to find tail positions
+    contents |> Enum.map(fn move -> unwrapMoves(move) end)
                                                          |> Enum.flat_map(fn move -> move end)
                                                          |> Enum.reduce([0,0,0,0, %{"0,0" => 1}], fn (move, data) -> performMove(move, data) end)
-    map |> Map.keys()
-        |> length()
+                                                         |> Enum.reverse()
+                                                         |> hd()
+                                                         |> Map.keys()
+                                                         |> length()
   end
 
   def solve1 do
     {:ok, contents} = File.read("day9.txt")
     part1(String.split(contents,"\n"))
+  end
+
+  @doc """
+  Move head according to move
+
+  ## Examples
+
+      iex> Day9.moveHead("R", [0,0, []])
+      [1,0, [[1, 0]]]
+
+      iex> Day9.moveHead("R", [1,0, [[1, 0]]])
+      [2,0, [[2,0], [1, 0]]]
+
+      iex> Day9.moveHead("R", [2,0, [[2,0] , [1, 0]]])
+      [3,0, [[3,0],[2,0], [1, 0]]]
+
+      iex> Day9.moveHead("L", [0,0, []])
+      [-1,0, [[-1, 0]]]
+
+      iex> Day9.moveHead("L", [-1,0, [[-1, 0]]])
+      [-2,0, [[-2, 0], [-1, 0]]]
+
+      iex> Day9.moveHead("U", [0,0, []])
+      [0,1, [[0, 1]]]
+
+      iex> Day9.moveHead("U", [0,1, [[0, 1]]])
+      [0,2, [[0, 2], [0, 1]]]
+
+      iex> Day9.moveHead("D", [0,0, []])
+      [0,-1, [[0, -1]]]
+
+      iex> Day9.moveHead("D", [0,-1, [[0, -1]]])
+      [0,-2, [[0, -2], [0, -1]]]
+
+  """
+
+  def moveHead("R", [x,y, []]), do: [x+1,y, [[x+1,y]]]
+  def moveHead("R", [x,y, [head | tail]]), do: [x+1,y, [[x+1,y] , head | tail]]
+  def moveHead("L", [x,y, []]), do: [x-1,y, [[x-1,y]]]
+  def moveHead("L", [x,y, [head | tail]]), do: [x-1,y, [[x-1,y] , head | tail]]
+  def moveHead("U", [x,y, []]), do: [x,y+1, [[x,y+1]]]
+  def moveHead("U", [x,y, [head | tail]]), do: [x,y+1, [[x,y+1], head | tail]]
+  def moveHead("D", [x,y, []]), do: [x,y-1, [[x,y-1]]]
+  def moveHead("D", [x,y, [head | tail]]), do: [x,y-1, [[x,y-1], head | tail]]
+
+  @doc """
+  move a tail
+
+  ## Examples
+
+      Same coordinate, nothing in moves
+      iex> Day9.moveTail([0,0], [0,0, [[0,0]]])
+      [0,0, [[0,0]]]
+
+      Same coordinate, nothing in moves
+      iex> Day9.moveTail([-4,3], [-4,3, [[0,0]]])
+      [-4,3, [[0,0]]]
+
+      Same coordinate, with move inside
+      iex> Day9.moveTail([-7,-6], [-7,-6, [[0,1]]])
+      [-7,-6, [[0,1]]]
+
+      Same coordinate, with moves inside
+      iex> Day9.moveTail([-7,-6], [-7,-6, [[0,1], [5,6]]])
+      [-7,-6, [[0,1], [5,6]]]
+
+      Difference of one in any direction does not move
+      iex> Day9.moveTail([-6,-5], [-7,-5, [[0,1], [5,6]]])
+      [-7, -5, [[0, 1], [5, 6]]]
+
+      Difference diagonal of one will not move
+
+      Difference or two in primary directions goes one
+      iex> Day9.moveTail([2,0], [0,0,[[0,0]]])
+      [1,0, [[1,0], [0,0]]]
+
+      iex> Day9.moveTail([2,0], [0,0,[[7,-9]]])
+      [1,0, [[1,0], [7, -9]]]
+
+
+  """
+
+  #distance of zero
+  def moveTail([x,y], [x, y, [head | _tail = []]]), do: [x, y, [head]]
+  def moveTail([x,y], [x, y, [head | tail]]), do: [x, y, [head | tail]]
+  #distance of one
+  def moveTail([x1,y], [x2, y, [head | _tail = []]]) when x1 == x2 + 1, do: [x2, y, [head]]
+  def moveTail([x1,y], [x2, y, [head | tail]]) when x1 == x2 + 1, do: [x2, y, [head | tail]]
+  def moveTail([x1,y], [x2, y, [head | _tail = []]]) when x1 == x2 - 1, do: [x2, y, [head]]
+  def moveTail([x1,y], [x2, y, [head | tail]]) when x1 == x2 - 1, do: [x2, y, [head | tail]]
+
+  def moveTail([x,y1], [x, y2, [[head | _tail = []]]]) when y1 == y2 + 1, do: [x, y2, [head]]
+  def moveTail([x,y1], [x, y2, [head | tail]]) when y1 == y2 + 1, do: [x, y2, [head | tail]]
+  def moveTail([x,y1], [x, y2, [head | _tail = []]]) when y1 == y2 - 1, do: [x, y2, [head]]
+  def moveTail([x,y1], [x, y2, [head | tail]]) when y1 == y2 - 1, do: [x, y2, [head | tail]]
+
+  def moveTail([x1,y1], [x2, y2, [head | _tail = []]]) when x1 == x2 + 1 and y1 == y2 + 1, do: [x2, y2, [head]]
+  def moveTail([x1,y1], [x2, y2, [head | tail]]) when x1 == x2 + 1 and y1 == y2 + 1, do: [x2, y2, [head | tail]]
+  def moveTail([x1,y1], [x2, y2, [head | _tail = []]]) when x1 == x2 + 1 and y1 == y2 - 1, do: [x2, y2, [head]]
+  def moveTail([x1,y1], [x2, y2, [head | tail]]) when x1 == x2 + 1 and y1 == y2 - 1, do: [x2, y2, [head | tail]]
+  def moveTail([x1,y1], [x2, y2, [head | _tail = []]]) when x1 == x2 - 1 and y1 == y2 - 1, do: [x2, y2, [head]]
+  def moveTail([x1,y1], [x2, y2, [head | tail]]) when x1 == x2 - 1 and y1 == y2 - 1, do: [x2, y2, [head | tail]]
+  def moveTail([x1,y1], [x2, y2, [head | _tail = []]]) when x1 == x2 - 1 and y1 == y2 + 1, do: [x2, y2, [head]]
+  def moveTail([x1,y1], [x2, y2, [head | tail]]) when x1 == x2 - 1 and y1 == y2 + 1, do: [x2, y2, [head | tail]]
+
+  #distance of two
+  def moveTail([x1,y], [x2, y, [head | _tail = []]]) when x1 == x2 + 2, do: [x2+1, y, [[x2+1,y], head]]
+  def moveTail([x1,y], [x2, y, [head | tail]]) when x1 == x2 + 2, do: [x2+1, y, [[x2+1,y], head | tail]]
+  def moveTail([x,y1], [x, y2, [head | _tail = []]]) when y1 == y2 + 2, do: [x, y2 + 1, [[x,y2 + 1], head]]
+  def moveTail([x,y1], [x, y2, [head | tail]]) when y1 == y2 + 2, do: [x, y2 + 1, [[x,y2 + 1], head | tail]]
+  def moveTail([x,y1], [x, y2, [head | _tail = []]]) when y1 == y2 - 2, do: [x, y2 - 1, [[x,y2 - 1], head]]
+  def moveTail([x,y1], [x, y2, [head | tail]]) when y1 == y2 - 2, do: [x, y2 - 1, [[x,y2 - 1], head | tail]]
+  def moveTail([x1,y], [x2, y, [head | _tail = []]]) when x1 == x2 - 2, do: [x2-1, y, [[x2-1,y], head]]
+  def moveTail([x1,y], [x2, y, [head | tail]]) when x1 == x2 - 2, do: [x2-1, y, [[x2-1,y], head | tail]]
+
+  #diagnonals (should be at least four sets -- so 8)
+   #1 and #2
+  def moveTail([x1,y1], [x2, y2, [head | _tail = []]]) when x1 == x2 - 2 and y1 == y2 + 1 or x1 == x2 - 1 and y1 == y2 + 2 or x1 == x2 - 2 and y1 == y2 + 2, do: [x2-1,y2+1, [[x2-1,y2+1], head]]
+  def moveTail([x1,y1], [x2, y2, [head | tail]])       when x1 == x2 - 2 and y1 == y2 + 1 or x1 == x2 - 1 and y1 == y2 + 2 or x1 == x2 - 2 and y1 == y2 + 2, do: [x2-1,y2+1, [[x2-1,y2+1], head | tail]]
+  #3 and #4
+  def moveTail([x1,y1], [x2, y2, [head | _tail = []]]) when x1 == x2 + 1 and y1 == y2 + 2 or x1 == x2 + 2 and y1 == y2 + 1 or x1 == x2 + 2 and y1 == y2 + 2, do: [x2+1,y2+1, [[x2+1,y2+1], head]]
+  def moveTail([x1,y1], [x2, y2, [head | tail]])       when x1 == x2 + 1 and y1 == y2 + 2 or x1 == x2 + 2 and y1 == y2 + 1 or x1 == x2 + 2 and y1 == y2 + 2, do: [x2+1,y2+1, [[x2+1,y2+1], head | tail]]
+  #5 and #6
+  def moveTail([x1,y1], [x2, y2, [head | _tail = []]]) when x1 == x2 + 2 and y1 == y2 - 1 or x1 == x2 + 1 and y1 == y2 - 2  or x1 == x2 + 2 and y1 == y2 - 2, do: [x2+1,y2-1, [[x2+1,y2-1], head]]
+  def moveTail([x1,y1], [x2, y2, [head | tail]])       when x1 == x2 + 2 and y1 == y2 - 1 or x1 == x2 + 1 and y1 == y2 - 2 or x1 == x2 + 2 and y1 == y2 - 2, do: [x2+1,y2-1, [[x2+1,y2-1], head | tail]]
+  #7 and #8
+  def moveTail([x1,y1], [x2, y2, [head | _tail = []]]) when x1 == x2 - 1 and y1 == y2 - 2 or x1 == x2 - 2 and y1 == y2 - 1  or x1 == x2 - 2 and y1 == y2 - 2, do: [x2-1,y2-1, [[x2-1,y2-1], head]]
+  def moveTail([x1,y1], [x2, y2, [head | tail]])       when x1 == x2 - 1 and y1 == y2 - 2 or x1 == x2 - 2 and y1 == y2 - 1 or x1 == x2 - 2 and y1 == y2 - 2, do: [x2-1,y2-1, [[x2-1,y2-1], head | tail]]
+
+    #repeat the proper number of times
+    #for position 9's only count unique --> reduce to map with keys of x,y and count.
+    #then sum it.
+
+    #call a catch up move with leader and one --> place result into position map
+    #then one and two --> place two into position map
+    #....
+    #then eight and nine --> place nine into postion map
+    #update nine's visited --> should only place coordinates
+  #end
+
+
+  def repeatMoveTail(moves, 9), do: moves
+  def repeatMoveTail(moves, count) do
+    moves |> Enum.reduce([0,0, [[0,0]]], fn (position, positions) -> moveTail(position, positions) end)
+          |> Enum.reverse()
+          |> hd()
+          |> Enum.reverse()
+          |> repeatMoveTail(count + 1)
+  end
+
+  @doc """
+  Part 2 moves 9 tails
+
+  ## Example
+
+      iex> Day9.part2(["R 5","U 8","L 8","D 3","R 17","D 10","L 25","U 20"])
+      36
+
+  """
+
+  def part2(contents) do
+    contents |> Enum.map(fn move -> unwrapMoves(move) end)
+                                 |> Enum.flat_map(fn move -> move end)
+                                 |> Enum.reduce([0,0, [[0,0]]], fn (move, positions) -> moveHead(move, positions) end)
+                                 |> Enum.reverse() # get positions to top
+                                 |> hd() # grab positions
+                                 |> Enum.reverse() # flip to correct order
+                                 |> repeatMoveTail(0) # calculate all tails
+                                 |> Enum.sort() # get like coordinates together
+                                 |> Enum.chunk_by(fn x -> x end) # duplicates get collected
+                                 |> length() # length of unique
+  end
+
+  def solve2 do
+    {:ok, contents} = File.read("day9.txt")
+    part2(String.split(contents,"\n"))
   end
 end
