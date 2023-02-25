397e294869d847e0bb41cb77de22364810c0681d
Jason Hite <hite77@gmail.com>
2023-02-25 15:11:43 -0500

    Day 12 part 1

--- day12/lib/day12.ex
+++ day12/lib/day12.ex
@@ -15,4 +15,112 @@ defmodule Day12 do
   def hello do
     :world
   end
+
+  def find(maps, character, [yextents, xextents], [y,xextents]), do: find(maps, character, [yextents, xextents], [y+1,0])
+  def find(maps, character, [yextents, xextents], [y,x]) do
+    if maps[y][x] == character do
+      [y,x]
+    else
+      find(maps, character, [yextents, xextents], [y,x+1])
+    end
+  end
+
+  def parse(contents) do
+    maps = String.split(contents, "\n", trim: true)  |> Enum.map(fn line -> String.graphemes(line) end)
+                                                     |> Enum.map(fn line->  Stream.with_index(line, 0) |> Enum.reduce(%{}, fn({v,k}, acc)-> Map.put(acc, k, v) end) end)
+                                                     |> Stream.with_index(0) |> Enum.reduce(%{}, fn({v,k}, acc)-> Map.put(acc, k, v) end)
+    yextents = length(Map.keys(maps))
+    xextents = length(Map.keys(maps[0]))
+    [find(maps,"S", [yextents, xextents], [0,0]), maps]
+  end
+
+  def move(_map, _from, [-1, _tox], _extents), do: false
+  def move(_map, _from, [_toy, -1], _extents), do: false
+  def move(_map, _from, [_toy,  x], [_extenty, x]), do: false
+  def move(_map, _from, [y,  _tox], [y, _extentx]), do: false
+  def move(map, [fromy, fromx], [toy, tox], _extents) do
+    <<from::utf8>> = map[fromy][fromx] # might be S
+    <<to::utf8>> = map[toy][tox]
+
+    cond do
+      (to == ?E and ((?z - from) < 2)) -> :solved
+      to == ?E                         -> (?z - from) < 2
+      from == ?S                       -> (to - ?a) < 2
+      true                             -> (to - from) < 2
+    end
+  end
+
+  def tryDirection([y,x], [y2,x2], extents, map, from, to, moves, visited, solved) do
+    canMove = move(map, [y, x], [y2, x2], extents)
+    cond do
+      solved             -> [true, to, moves, visited]
+      [y2,x2] in visited -> [false, to, moves, visited]
+       canMove == true    -> addMove([y2, x2], to, from, moves, visited)
+       canMove == :solved -> [_solved, to, moves, visited] = addMove([y2, x2], to, from, moves, visited)
+                             [true, to-1, moves, visited]
+       true               -> [false, to, moves, visited]
+    end
+  end
+
+  def calculateMoves(true, _map, _key, moves, visited), do: [true, moves, visited]
+  def calculateMoves(false, map, key, moves, visited) do
+    yextents = length(Map.keys(map))
+    xextents = length(Map.keys(map[0]))
+    extents = [yextents, xextents]
+    move = moves[key]
+    [y, x] = move |> hd()
+    to = (Map.keys(moves) |> Enum.sort() |> Enum.reverse() |> hd()) + 1
+
+    #up
+    [solved, to, moves, visited] = tryDirection([y, x], [y-1,x], extents, map, key, to, moves, visited, false)
+    #right
+    [solved, to, moves, visited] = tryDirection([y, x], [y, x + 1], extents, map, key, to, moves, visited, solved)
+    #down
+    [solved, to, moves, visited] = tryDirection([y, x], [y+1, x], extents, map, key, to, moves, visited, solved)
+    #left
+    [solved, to, moves, visited] = tryDirection([y, x], [y, x - 1], extents, map, key, to, moves, visited, solved)
+
+    moves = Map.delete(moves, key)
+    case solved do
+      true -> [true, moves[to], visited]
+      false -> [false, moves, visited]
+    end
+  end
+
+  def addMove(move, to, from, moves, [head|tail]) do
+    [head2| tail2] = moves[from]
+    moves = put_in(moves[to], [move, head2 | tail2])
+    [false, to + 1, moves, [move, head | tail]]
+  end
+
+  def loopKeys(true, _map, _keys, moves, visited), do: [true, moves, visited]
+  def loopKeys(false, _map, [], moves, visited), do: [false, moves, visited]
+  def loopKeys(false, map, [head | tail], moves, visited) do
+    [solved, moves, visited] = calculateMoves(false, map, head, moves, visited)
+    loopKeys(solved, map, tail, moves, visited)
+  end
+
+  def calculateMoves(true, _map, moves, _visited), do: moves
+  def calculateMoves(false, map, moves, visited) do
+    [solved, moves, visited] = loopKeys(false, map, Map.keys(moves), moves, visited)
+    calculateMoves(solved, map, moves, visited)
+  end
+
+  def part1(contents) do
+    [start, map] = parse(contents)
+    (calculateMoves(false, map, %{0 => [start]}, [start]) |> Enum.count()) - 1
+  end
+
+  def part2(contents) do
+    [_start, map] = parse(contents)
+    # find all a's --> coords list
+    # map over then and call calculateMoves and then count and output the counts
+    # sort and return smallest hd() after sort
+
+  end
+
+  def solve1 do
+    {:ok, contents} = File.read("Day12.txt")
+    part1(contents)
+  end
 end
--- day12/test/day12_test.exs
+++ day12/test/day12_test.exs
@@ -2,7 +2,88 @@ defmodule Day12Test do
   use ExUnit.Case
   doctest Day12
 
-  test "greets the world" do
-    assert Day12.hello() == :world
+  setup_all do
+    contents = """
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+"""
+    map = %{0 => %{0 => "S",1 => "a", 2 => "b", 3 => "q", 4 => "p", 5 => "o", 6 => "n", 7 => "m"},
+            1 => %{0 => "a", 1 => "b", 2 => "c", 3 => "r", 4 => "y", 5 => "x", 6 => "x", 7 => "l"},
+            2 => %{0 => "a", 1 => "c", 2 => "c", 3 => "s", 4 => "z", 5 => "E", 6 => "x", 7 => "k"},
+            3 => %{0 => "a", 1 => "c", 2 => "c", 3 => "t", 4 => "u", 5 => "v", 6 => "w", 7 => "j"},
+            4 => %{0 => "a", 1 => "b", 2 => "d", 3 => "e", 4 => "f", 5 => "g", 6 => "h", 7 => "i"}}
+    {:ok, part1: contents, map: map}
+  end
+
+  test "parse", state do
+    [start, map] = Day12.parse(state[:part1])
+    assert start == [0,0]
+    assert Map.keys(map) == [0,1,2,3,4]
+    assert map[0] == %{0 => "S",1 => "a", 2 => "b", 3 => "q", 4 => "p", 5 => "o", 6 => "n", 7 => "m"}
+    assert map[1] == %{0 => "a", 1 => "b", 2 => "c", 3 => "r", 4 => "y", 5 => "x", 6 => "x", 7 => "l"}
+    assert map[2] == %{0 => "a", 1 => "c", 2 => "c", 3 => "s", 4 => "z", 5 => "E", 6 => "x", 7 => "k"}
+    assert map[3] == %{0 => "a", 1 => "c", 2 => "c", 3 => "t", 4 => "u", 5 => "v", 6 => "w", 7 => "j"}
+    assert map[4] == %{0 => "a", 1 => "b", 2 => "d", 3 => "e", 4 => "f", 5 => "g", 6 => "h", 7 => "i"}
+  end
+
+  test "move", state do
+    assert Day12.move(state[:map], [0,0], [0,1], [5, 8]) == true #S is a
+    assert Day12.move(state[:map], [1,0], [1,1], [5, 8]) == true
+    assert Day12.move(state[:map], [2,1], [2,0], [5, 8]) == true
+    assert Day12.move(state[:map], [0,0], [-1,0], [5, 8]) == false
+    assert Day12.move(state[:map], [0,0], [0,-1], [5, 8]) == false
+    assert Day12.move(state[:map], [0,7], [0,8], [5, 8]) == false
+    assert Day12.move(state[:map], [4,7], [5,7], [5, 8]) == false
+    assert Day12.move(state[:map], [2,4], [2,5], [5, 8]) == :solved
+    assert Day12.move(state[:map], [1,5], [2,5], [5, 8]) == false #E is z
+  end
+
+  test "add moves from one to two" do
+    [solved, to, moves, visited] = Day12.addMove([0,1], 1, 0, %{0 => [[0,0]]}, [[0,0]])
+    assert solved == false
+    assert to == 2
+    assert visited == [[0,1],[0,0]]
+    assert moves == %{0 => [[0,0]], 1 => [[0,1],[0,0]]}
+  end
+
+  test "add moves from two to more" do
+    [solved, to, moves, visited] = Day12.addMove([1,0], 2, 1, %{0 => [[0, 0]], 1 => [[0, 1], [0, 0]]}, [[0, 1], [0, 0]])
+    assert solved == false
+    assert to == 3
+    assert visited == [[1,0],[0,1],[0,0]]
+    assert moves == %{0 => [[0,0]], 1 => [[0,1],[0,0]], 2 => [[1, 0], [0, 1], [0, 0]]}
+  end
+
+
+  test "calculateMoves adds move to visited and moves", state do
+    [solved, moves, visited] = Day12.calculateMoves(false, state[:map], 0, %{0 => [[0,0]]}, [[0,0]])
+    assert solved == false
+    assert moves ==  %{1 => [[0, 1], [0, 0]], 2 => [[1, 0], [0, 0]]}
+    assert visited ==  [[1,0],[0,1],[0,0]]
+  end
+
+  test "calculateMoves doesn't add moves if visited", state do
+    [solved, moves, visited] = Day12.calculateMoves(false, state[:map], 0, %{0 => [[0,0]]}, [[0,0],[0,1]])
+    assert solved == false
+    assert moves ==  %{1 => [[1, 0], [0, 0]]}
+    assert visited ==  [[1, 0], [0, 0], [0, 1]]
+  end
+
+   test "calculateMoves returns moves when solved", state do
+    [solved, moves, visited] = Day12.calculateMoves(false, state[:map], 0, %{0 => [[2,4]]}, [[2,4]])
+    assert solved == true
+    assert moves ==  [[2, 5], [2, 4]]
+    assert visited == [[2, 5], [1, 4], [2, 4]]
+  end
+
+  test "part1", state do
+    assert Day12.part1(state[:part1]) == 31
+  end
+
+  test "solutions" do
+    assert Day12.solve1 == 462
   end
 end
